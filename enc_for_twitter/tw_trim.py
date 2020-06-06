6dfcf094daa5a5a078b87482f661331660a5563f
かた <61326119+dj-kata@users.noreply.github.com>
2020-06-06 18:08:10 +0900

    コードを少し整理。最大長をパラメータとした。

--- enc_for_twitter/tw_trim.py
+++ enc_for_twitter/tw_trim.py
@@ -5,9 +5,13 @@
 # 2pass目で検出された位置+αまでをエンコード
 import subprocess, sys, re, math, os
 
+### パラメータ
 RESULT_DURATION = 5 # リザルト画面(厳密には、最初のsc点直後)を何秒間残すか
 SC_IDX = 0  # 何番目のシーンチェンジ時刻をベースにするか。デフォルトは0。
-SC_THRESHOLD = 0.8 # シーンチェンジ判定のしきい値
+SC_THRESHOLD = 0.8  # シーンチェンジ判定のしきい値
+MAX_DURATION = 140 # 動画の最大長(s)、twitterだと140
+
+### 引数の処理
 if len(sys.argv) < 4:
     print ('usage: %s input.mp4 [h:]m:s output_name' % sys.argv[0])
     sys.exit(1)
@@ -25,14 +29,14 @@ if len(st) == 3:
 cmd_base = f'./ffmpeg.exe -ss {st_sec} -i {f_in} -c:v h264_nvenc'
 
 ### 1pass目
-cmd = f'{cmd_base} -t 140 -filter:v "select=\'gt(scene,{SC_THRESHOLD})\',showinfo" -f null /dev/null'
+cmd = f'{cmd_base} -t {MAX_DURATION} -filter:v "select=\'gt(scene,{SC_THRESHOLD})\',showinfo" -f null /dev/null'
 ###### encode実行
 proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 ###### シーンチェンジ情報を分析
 dat = [l.decode() for l in proc.stderr.readlines() if 'pts_time' in l.decode()]
 sc = [math.ceil(float(re.sub(' .*', '', re.sub('.*pts_time:', '', d)))) for d in dat]
-duration = min(140, sc[SC_IDX] + RESULT_DURATION)
+duration = min({MAX_DURATION}, sc[SC_IDX] + RESULT_DURATION)
 
 ### 2pass目
 fadeout  = f'-filter:v "fade=out:st={duration-0.5}:d=0.5" '
@@ -40,4 +44,3 @@ fadeout += f'-filter:a "afade=t=out:st={duration-0.5}:d=0.5"'
 print(fadeout)
 cmd = f'{cmd_base} -t {duration} {fadeout} {f_out}'
 os.system(cmd)
-
